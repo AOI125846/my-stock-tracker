70e2dfca2c3ffd40a4921d8293f6e3d6326cbfeb
AOI125846 <a0583247689@gmail.com>
2026-01-02 11:57:24 +0200

    Delete tests directory

--- tests/test_tracker.py
+++
@@ -1,7 +0,0 @@
-from src.my_stock_tracker.tracker import fetch_chart_data
-
-def test_fetch_stock_demo():
-    res = fetch_chart_data("TST", "1d")
-    assert isinstance(res, list)
-    assert len(res) > 0
-    assert "close" in res[0]
